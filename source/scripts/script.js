bfcbb68287a190e3b53117e8f6b66c5a9afc709f
Roman Yakovlev <tensegrity666@eicloud.com>
2020-01-23 23:58:08 +0300

    Completes validation of guests and rooms inputs

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,27 +1,32 @@
 'Use strict';
 
-const MAX_GUESTS_PER_ROOM = 3;
+const MAX_GUESTS_PER_ROOM = 2;
+const GUESTS_MIN = 1;
+const GUESTS_MAX = 10;
+const GUESTS_DEFAULT = 2;
 
-const formElement = document.forms['search-form'];
+{
+  const formElement = document.forms['search-form'];
 
-const guests = formElement['guests'];
-const rooms = formElement['rooms'];
-const dateIn = formElement['date-in'];
-const dateOut = formElement['date-out'];
-const buttonSearch = formElement['search-button'];
+  const guests = formElement['guests'];
+  const rooms = formElement['rooms'];
+  const dateIn = formElement['date-in'];
+  const dateOut = formElement['date-out'];
+  const buttonSearch = formElement['search-button'];
 
-guests.min = 1;
-guests.max = 6;
-guests.required = true;
+  guests.min = GUESTS_MIN;
+  guests.max = GUESTS_MAX;
+  guests.value = GUESTS_DEFAULT;
 
-const setRoomsNumber = (rooms, guests) => {
-  rooms.min = Math.ceil(guests / MAX_GUESTS_PER_ROOM);
-  rooms.max = guests;
+  const setRoomsNumber = (rooms, guests) => {
+    rooms.min = Math.ceil(guests / MAX_GUESTS_PER_ROOM);
+    rooms.max = guests;
+    rooms.value = rooms.min;
+  }
+
+  setRoomsNumber(rooms, guests.value);
+
+  guests.onchange = () => {
+    setRoomsNumber(rooms, guests.value);
+  }
 }
-
-guests.value = 2;
-
-setRoomsNumber(rooms, guests.value);
-
-rooms.value = rooms.min;
-
